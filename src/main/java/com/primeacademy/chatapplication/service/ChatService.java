8a8cb80ebe96c7c8c132e703bf9fcdf616802604
Axyonov Vladimir <goodbye670@gmail.com>
2024-06-24 10:18:54 +0500

    code cleaning

--- src/main/java/com/primeacademy/chatapplication/service/ChatService.java
+++ src/main/java/com/primeacademy/chatapplication/service/ChatService.java
@@ -37,17 +37,14 @@ public class ChatService {
         User sender = userRepository.findByUsername(senderUsername)
                 .orElseThrow(() -> new RuntimeException("Sender not found"));
 
-        // Получение всех получателей
         List<User> receivers = receiverUsernames.stream()
                 .map(username -> userRepository.findByUsername(username)
                         .orElseThrow(() -> new RuntimeException("Receiver " + username + " not found")))
                 .toList();
 
-        // Включение отправителя в список участников
         Set<User> participants = new HashSet<>(receivers);
         participants.add(sender);
 
-        // Проверка наличия комнаты чата, где участвуют только эти участники
         Optional<ChatRoom> existingChatRoom = chatRoomRepository.findAll().stream()
                 .filter(chatRoom -> {
                     Set<User> chatRoomUsers = chatRoom.getUsers();
@@ -64,11 +61,10 @@ public class ChatService {
             chatRoomRepository.save(chatRoom);
         }
 
-        // Создание и сохранение сообщения для каждого получателя
         for (User receiver : receivers) {
             Message message = new Message();
             message.setSender(sender);
-            message.setReceiver(receiver);  // Устанавливаем receiver
+            message.setReceiver(receiver);
             message.setContent(content);
             message.setTimestamp(LocalDateTime.now());
             message.setChatRoom(chatRoom);
